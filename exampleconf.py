18998ce7f12146121c7daa3b3306d327c38f1251
bobobo1618 <bobobo1618@gmail.com>
2015-05-09 13:12:24 -0700

    Add example conf.py

---
+++ exampleconf.py
@@ -0,0 +1,10 @@
+storage_host = 'http://thing.com'
+fullbucket = 'full'
+thumbbucket = 'thumbs'
+
+fullprefix = '/'.join((storage_host, fullbucket))
+thumbprefix = '/'.join((storage_host, thumbbucket))
+
+client_id = 'clientid'
+client_secret = 'secret'
+
